53f135109403aabc4f1e65bbf93f958a1e9d1982
Patrick Jung <patrick.pddstudio@googlemail.com>
2020-05-14 23:11:51 +0200

    Added export declarations for new api modules

---
+++ src/internal/api-modules/index.ts
@@ -0,0 +1,6 @@
+export * from './achievements-module';
+export * from './ranking-module';
+export * from './ribbons-module';
+export * from './updates-module';
+export * from './userweapons-module';
+export * from './webshop-module';
