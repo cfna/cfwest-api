7fd107f42c9f2eb0d5fa41fde73ab73f156e7a94
Patrick Jung <patrick.pddstudio@googlemail.com>
2020-05-14 22:57:30 +0200

    Added new ribbons api module implementation

---
+++ src/internal/api-modules/ribbons-module.ts
@@ -0,0 +1,43 @@
+import { BaseApiModule } from './base-module';
+import { UserRibbonResponse, UserRibbon, RibbonCountInfo, Ribbon } from '../api-models';
+
+export class RibbonsApiModule extends BaseApiModule {
+  private fetchUserRibbonInfo(playerUSN: string): Promise<UserRibbonResponse | undefined> {
+    return this.httpClient.get<UserRibbonResponse>('userribbons.json', {
+      params: {
+        usn: playerUSN,
+      },
+    });
+  }
+
+  public async getRibbonList(): Promise<Ribbon[] | undefined> {
+    return this.httpClient.get<Ribbon[]>('ribbonslist.json');
+  }
+
+  public async getUserRibbons(playerUSN: string): Promise<UserRibbon[] | undefined> {
+    const response = await this.fetchUserRibbonInfo(playerUSN);
+    if (response && response.userRibbonList) {
+      return response.userRibbonList;
+    }
+    return undefined;
+  }
+
+  public async getUserRibbonsCount(playerUSN: string): Promise<RibbonCountInfo | undefined> {
+    const response = await this.fetchUserRibbonInfo(playerUSN);
+    if (response && response.ribbonCount) {
+      return response.ribbonCount;
+    }
+    return undefined;
+  }
+
+  public async getUserHasHonorableSoldier(playerUSN: string): Promise<boolean | undefined> {
+    const response = await this.fetchUserRibbonInfo(playerUSN);
+    if (response && response.hasHGW) {
+      const hgwInfo = response.hasHGW[0];
+      if (hgwInfo.hasRibbon) {
+        return hgwInfo.hasRibbon === 1;
+      }
+    }
+    return undefined;
+  }
+}
