884d7808b2426ee2c35df7c80eb89ec80174af17
Patrick Jung <patrick.pddstudio@googlemail.com>
2020-05-15 21:33:11 +0200

    Removed outdated code

--- src/models/base.ts
+++
@@ -1,9 +0,0 @@
-export interface BaseResponse {
-  APIresult: string;
-  APImessage: string;
-}
-
-export interface CountableResponse extends BaseResponse {
-  Total_Count: number;
-  Count: number;
-}
--- src/utils/stringEnum.ts
+++
@@ -1,6 +0,0 @@
-export function stringEnum<T extends string>(o: Array<T>): { [K in T]: K } {
-  return o.reduce((res, key) => {
-    res[key] = key;
-    return res;
-  }, Object.create(null));
-}
