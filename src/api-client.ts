780f5ed57ccbbf2b05c0f6c357cde044151e08ee
Patrick Jung <patrick.pddstudio@googlemail.com>
2020-05-15 21:50:03 +0200

    Added documentation snippets for new ApiClient implementation

--- src/api-client.ts
+++ src/api-client.ts
@@ -19,14 +19,44 @@ const defaultApiModuleOptions = {
   weaponCategoryMapper: new WeaponCategoryMapperImpl()
 };
 
+/**
+ * This class is the core component to communicate with [CrossFire West](https://crossfire.z8games.com/) Services.
+ */
 export class ApiClient {
+  /**
+   * Property to access the [[AchievementsApiModule]]
+   */
   public readonly achievements: AchievementsApiModule;
+
+  /**
+   * Property to access the [[RankingApiModule]]
+   */
   public readonly ranking: RankingApiModule;
+
+  /**
+   * Property to access the [[RibbonsApiModule]]
+   */
   public readonly ribbons: RibbonsApiModule;
+
+  /**
+   * Property to access the [[UpdatesApiModule]]
+   */
   public readonly updates: UpdatesApiModule;
+
+  /**
+   * Property to access the [[UserWeaponsApiModule]]
+   */
   public readonly userWeapons: UserWeaponsApiModule;
+
+  /**
+   * Property to access the [[WebShopApiModule]]
+   */
   public readonly webshop: WebShopApiModule;
 
+  /**
+   * Constructor to initiale an [[ApiClient]] instance.
+   * @param options Optional options to configure the [[ApiClient]]
+   */
   constructor(options?: ApiClientOptions) {
     const initOptions = this.mergeConstructorOptions(options);
     const moduleInitOptions = this.createApiModuleOptions(initOptions);
@@ -47,6 +77,12 @@ export class ApiClient {
   }
 }
 
+/**
+ * Options to modify [[ApiClient]]s behavior.
+ */
 export interface ApiClientOptions {
+  /**
+   * Specify the [[HttpClient]] implementation to use.
+   */
   httpClient: HttpClient;
 }
