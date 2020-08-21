2d315a70bcc64e461f3aaf71d92593331b4ff2e1
Patrick Jung <patrick.pddstudio@gmail.com>
2020-08-21 12:10:25 +0200

    fix: Updated failing test case for user profile

--- tests/modules/userprofile.spec.ts
+++ tests/modules/userprofile.spec.ts
@@ -1,6 +1,6 @@
 jest.unmock('axios');
 
-import ApiClient, { UserProfileResponse } from '../../dist';
+import ApiClient, { UserProfileResponse } from '../../src';
 import * as TestUtils from '../utils';
 
 jest.setTimeout(90000);
@@ -9,12 +9,11 @@ const logger = TestUtils.getLogger();
 
 describe(TestUtils.formatTestSuiteTitle('UserProfile'), () => {
   test('User profile query should return result', async (done) => {
-    expect.assertions(1);
+    expect.assertions(2);
     const api = new ApiClient();
-    const result: UserProfileResponse = await api.userProfile.getUserProfile('1');
+    const result: UserProfileResponse | undefined = await api.userProfile.getUserProfile('1');
     expect(result).toBeDefined();
-
-    logger.info(`Requested profile information for user: ${result.dsProfileDetails[0].DISPLAY_NAME}`);
+    expect(result!.dsProfileDetails.length).toEqual(1);
 
     done();
   });
