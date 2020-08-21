f315f66ccf226d7a4513fdcf022bba6332e91f14
Patrick Jung <patrick.pddstudio@gmail.com>
2020-08-21 12:29:53 +0200

    Re-formatted tests without semicolons

--- tests/modules/userprofile.spec.ts
+++ tests/modules/userprofile.spec.ts
@@ -1,20 +1,20 @@
-jest.unmock('axios');
+jest.unmock('axios')
 
-import ApiClient, { UserProfileResponse } from '../../src';
-import * as TestUtils from '../utils';
+import ApiClient, { UserProfileResponse } from '../../src'
+import * as TestUtils from '../utils'
 
-jest.setTimeout(90000);
+jest.setTimeout(90000)
 
-const logger = TestUtils.getLogger();
+const logger = TestUtils.getLogger()
 
 describe(TestUtils.formatTestSuiteTitle('UserProfile'), () => {
   test('User profile query should return result', async (done) => {
-    expect.assertions(2);
-    const api = new ApiClient();
-    const result: UserProfileResponse | undefined = await api.userProfile.getUserProfile('1');
-    expect(result).toBeDefined();
-    expect(result!.dsProfileDetails.length).toEqual(1);
+    expect.assertions(2)
+    const api = new ApiClient()
+    const result: UserProfileResponse | undefined = await api.userProfile.getUserProfile('1')
+    expect(result).toBeDefined()
+    expect(result!.dsProfileDetails.length).toEqual(1)
 
-    done();
-  });
-});
+    done()
+  })
+})
--- tests/modules/userweapons.spec.ts
+++ tests/modules/userweapons.spec.ts
@@ -1,23 +1,23 @@
-jest.unmock('axios');
+jest.unmock('axios')
 
-import ApiClient, { WeaponCategory } from '../../dist';
-import * as TestUtils from '../utils';
+import ApiClient, { WeaponCategory } from '../../dist'
+import * as TestUtils from '../utils'
 
-jest.setTimeout(90000);
+jest.setTimeout(90000)
 
-const logger = TestUtils.getLogger();
+const logger = TestUtils.getLogger()
 
 describe(TestUtils.formatTestSuiteTitle('UserWeapons'), () => {
   test('User weapons query should return result', async (done) => {
-    expect.assertions(2);
-    const api = new ApiClient();
-    const result = await api.userWeapons.getUserWeapons(TestUtils.DEFAULT_PLAYER_USN, 'sniper', 'permanent', 1, 10);
-    expect(result).toBeDefined();
-    expect(result!.length).toBeGreaterThanOrEqual(10);
+    expect.assertions(2)
+    const api = new ApiClient()
+    const result = await api.userWeapons.getUserWeapons(TestUtils.DEFAULT_PLAYER_USN, 'sniper', 'permanent', 1, 10)
+    expect(result).toBeDefined()
+    expect(result!.length).toBeGreaterThanOrEqual(10)
     result!.map((weapon, index) => {
-      const number = ++index;
-      logger.debug(`#${number} => ${weapon.DISPLAY_NAME}`);
-    });
-    done();
-  });
-});
+      const number = ++index
+      logger.debug(`#${number} => ${weapon.DISPLAY_NAME}`)
+    })
+    done()
+  })
+})
