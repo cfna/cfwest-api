ca74d8f471cf1674e14b6c910a1929a711a3b7fe
Patrick Jung <patrick.jung@externals.adidas.com>
2022-09-05 02:53:59 +0200

    Added test to common package

--- packages/common/test/http/http.client.spec.ts
+++ packages/common/test/http/http.client.spec.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '../../src'
 
 describe('HttpClient test suite', () => {
-
-    test('Instance should exist', () => {
-        const sut = new HttpClient()
-        expect(sut).toBeDefined()
-    })
+  test('Instance should exist', () => {
+    const sut = new HttpClient()
+    expect(sut).toBeDefined()
+  })
 })
