0ce3bf3cd9f9083c3eebb0ab844be09585c8a3d1
Patrick Jung <patrick.jung@externals.adidas.com>
2022-09-05 02:01:42 +0200

    Added common package for shared code

---
+++ packages/common/src/http/http.client.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance } from 'axios'
+import { Agent } from 'https'
+
+export class HttpClient {
+  private readonly httpClient: AxiosInstance
+
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: 'http://crossfire.z8games.com/rest',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9,de;q=0.8',
+        Host: 'crossfire.z8games.com',
+        Referer: 'http://crossfire.z8games.com/playerranking.html',
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36'
+      },
+      httpsAgent: new Agent({
+        rejectUnauthorized: false
+      })
+    })
+  }
+
+  async get<T>(url: string, options?: any): Promise<T | undefined> {
+    const response = await this.httpClient.get<T>(url, options)
+    if (response && response.data) {
+      return response.data
+    }
+    return undefined
+  }
+}
